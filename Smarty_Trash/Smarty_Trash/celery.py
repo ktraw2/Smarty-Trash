624ec0f78f43cda1d120e27f3d6123aa2a32584d
ktraw <ktraw2@gmail.com>
2018-11-26 22:01:56 -0800

    Configure periodic Celery tasks

--- Smarty_Trash/Smarty_Trash/celery.py
+++ Smarty_Trash/Smarty_Trash/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Smarty_Trash.settings')
@@ -14,7 +15,7 @@ app = Celery('Smarty_Trash')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
--- Smarty_Trash/data/tasks.py
+++ Smarty_Trash/data/tasks.py
@@ -1,23 +1,35 @@
 # Create your tasks here
+# Imports for Celery
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+from celery.schedules import crontab
+from celery.task import periodic_task
+# Imports for sensors
 from .sensors import *
 import time
 from .models import *
 from datetime import datetime
 
 
-@shared_task()
+@periodic_task(
+    run_every=(crontab(minute="*/1")),
+    name="poll_breakbeam",
+    ignore_result=True
+)
 def poll_breakbeam():
     breakbeam = BreakBeamSensor()
-    while True:
+    while False:
         if breakbeam.poll():
             entry = BreakBeam(event=datetime.now())
             entry.save()
         time.sleep(0.01)
 
 
-@shared_task()
+@periodic_task(
+    run_every=(crontab(minute="*/1")),
+    name="poll_magnetometer",
+    ignore_result=True
+)
 def poll_magnetometer():
     magnetometer = MagnetometerSensor()
     value = magnetometer.poll()
@@ -26,7 +38,11 @@ def poll_magnetometer():
         entry.save()
 
 
-@shared_task()
+@periodic_task(
+    run_every=(crontab(minute="*/1")),
+    name="poll_proximity",
+    ignore_result=True
+)
 def poll_proximity():
     proximity = ProximitySensor()
     value = proximity.poll()

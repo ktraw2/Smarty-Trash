a572b008dcfbf46b1c35678f30f0b67c33d21ad7
Kevin <ktraw2@users.noreply.github.com>
2018-11-29 02:55:09 +0000

    Disable DEBUG

--- Smarty_Trash/Smarty_Trash/settings.py
+++ Smarty_Trash/Smarty_Trash/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xlcmmn1)_uq*ez!qb&6((4utmt=pd7vqd1h7$*)%9=b9@$kxl2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

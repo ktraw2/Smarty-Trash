128215096494a84d02b52a78e5c72224ba1e6a6f
ktraw <ktraw2@gmail.com>
2018-11-30 16:38:39 -0800

    Fixed typo in permission configuration script

--- configureperms.sh
+++ configureperms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chmod +x ./runcelery.sh
-sudo chown pi:www-data Smarty-Trash/
-sudo chown pi:www-data Smarty-Trash/db.sqlite3
-chmod g+w Smarty-Trash/db.sqlite3
+sudo chown pi:www-data Smarty_Trash/
+sudo chown pi:www-data Smarty_Trash/db.sqlite3
+chmod g+w Smarty_Trash/db.sqlite3
